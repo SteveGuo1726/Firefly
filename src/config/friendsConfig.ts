0078052a548ca9c7a4be33332cccadcc86f9a873
Steve Guo <2367211699@qq.com>
2026-09-11 23:07:33 +0800

    Add new friend link configuration for Lin Blog

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -261,6 +261,18 @@ export const friendsConfig: FriendLink[] = [
 		],
 		weight: 24,
 		enabled: true
+	},
+	{
+		title: "Lin Blog",
+		imgurl: "https://linlog.top/api/uploads/2026/09/1788411216332767920-dca59196a965c5e8.jpg",
+		desc: "记录技术、互联网与日常观察",
+		siteurl: "https://linlog.top",
+		fetchFeed: true,
+		tags: [
+			"Blog"
+		],
+		weight: 25,
+		enabled: true
 	}
 ];
 
